480f6ec9492520b59dad1eebd4e49d5216bc4a45
Jordan Kasper <jordan@jordankasper.com>
2017-03-11 16:58:09 -0500

    Added Sass code and new thought JS

---
+++ src/js/new-thought.js
@@ -0,0 +1,35 @@
+(function() {
+
+    window.thoughter = window.thoughter || {};
+
+    /**
+     * Creates a new thought with the given text
+     *
+     * @param  {String} text  The text to use for the thought - this is REQUIRED!
+     * @return {Promise}      The resolved promise will have the new thought data object in it
+     */
+    window.thoughter.createThought = function createThought(text) {
+        if (typeof(text) !== 'string' || !text.length) {
+            return Promise.reject('Please provide text for this new thought');
+        }
+
+        return fetch(
+            'http://thoughter.herokuapp.com/api/Thoughts',
+            {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ content: text })
+            }
+        )
+        .then(function handleResponse(res) {
+            if (res.status > 299) {
+                console.error('Looks like a bad status code:', res.status);
+            } else {
+                return res.json()
+            }
+        });
+    };
+
+})();
