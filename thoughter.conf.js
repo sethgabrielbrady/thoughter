b1f195de9439c6b528928ab5f54eb44ed0b09082
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-16 13:35:42 -0400

    Everything is set up in the branch. Need to merge with master for the notes

---
+++ thoughter.conf.js
@@ -0,0 +1,14 @@
+odule.exports = function karmaConfig( config ){
+
+  config.set({
+    frameworks:[ 'mocha', 'chai'],
+    browsers: ['Chrome'],
+    singleRun: true,
+    files: [
+      'src/**/*.js',
+      'node_modules/sinon/pkg/sinon-2.0.0.js',//pulls in sinon
+      'tests/specs/**/*.js'
+    ]
+  });
+
+};
