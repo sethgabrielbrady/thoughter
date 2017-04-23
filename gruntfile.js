0d73fee4ea62346c7a1142010181d85c8579bc47
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-04-23 16:38:54 -0400

    organzied file structure

--- gruntfile.js
+++ gruntfile.js
@@ -1,108 +1,85 @@
 module.exports = function configuration(gruntConfig) {
-    gruntConfig.initConfig({
+  gruntConfig.initConfig({
 
+    clean:['build/'],
 
-        clean:['build/'],
-
-//copy
-        copy:{
-          allHTML:{
-            files:[
-              {
+    copy:{
+      allHTML:{
+          files:[
+            {
               cwd:'src',
               src:['*.html'],
               dest:'build/',
               expand:true
-              }
-            ]
-          },
-          allJavaScript:{
-            files:[
-              {
-              cwd:'src/js', //if sll your files are already in a subfolder, grunt i will copy it
-              src:['**/*.js'],//** means any sub directory
-              dest:'build/js/',
-              expand:true
-              }
-            ]
-          },
-          allJQuery:{
-            files:[
-              {
-              cwd:'node_modules/jquery/dist',
-              src:['jquery.js'],
-              dest:'build/js/vendor/',
-              expand:true
-              }
-            ]
+            }
+          ]
+      },
+      allJavaScript:{
+        files:[
+          {
+            cwd:'src/js', //if sll your files are already in a subfolder, grunt i will copy it
+            src:['**/*.js'],//** means any sub directory
+            dest:'build/js/',
+            expand:true
           }
-        },
-
-//sass
-        sass: {
-          allSASS:{
-            files:{
-              'build/style.css' : 'src/sass/main.scss'
-            }
+        ]
+      },
+      allJQuery:{
+        files:[
+          {
+            cwd:'node_modules/jquery/dist',
+            src:['jquery.js'],
+            dest:'build/js/vendor/',
+            expand:true
           }
-        },
+        ]
+      }
+    },
 
-//jshint
-        jshint:{
-          source:{
-            options:{
-              jshintrc:'.jshintrc'
-            },
-              files:{
-                  src:['src/**/*.js']
-              }
-            }
-          },
-
-//karma
-        karma:{
-          testUNITS:{
-            options:{
-                frameworks:[ 'mocha', 'chai'],
-                browsers: ['Chrome'],
-                singleRun: true,
-                files: [
-                  'src/**/*.js',
-                  'node_modules/fetch-mock/es5/client-browserified.js',
-                  'test/specs/**/*.js'
-                  ]
-                // preprocessors:{
-                //   'src/**/*.js': ['coverage'], //what files we are testing
-                // },
-                // reporters:['dots', 'coverage'],
-                // coverageReporter:{
-                // type:'text-summary'
-                // }
-              }
-            }
-
-          },
-
-//watch
-        watch: {
-          scripts: {
-            files: ['**/*.js', '**/*.html', 'src/sass/*.scss'],
-            tasks: ['jshint','karma', 'clean', 'copy', 'sass'],
-            // options: {
-            // spawn: false,
-            // event:['all']//what is this
-          // }
+    sass: {
+      allSASS:{
+        files:{
+          'build/style.css' : 'src/sass/main.scss'
         }
       }
+    },
 
+    jshint:{
+      source:{
+        options:{
+          jshintrc:'.jshintrc'
+        },
+        files:{
+          src:['src/**/*.js']
+        }
+      }
+    },
+
+    karma:{
+      testUNITS:{
+        options:{
+          frameworks:[ 'mocha', 'chai'],
+          browsers: ['Chrome'],
+          singleRun: true,
+          files: [
+            'src/**/*.js',
+            'node_modules/fetch-mock/es5/client-browserified.js',
+            'test/specs/**/*.js'
+          ]
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['**/*.js', '**/*.html', 'src/sass/*.scss'],
+        tasks: ['jshint','karma', 'clean', 'copy', 'sass'],
+      }
+    }
 
   });
 
+  require('load-grunt-tasks')(gruntConfig);
 
-//one task to rule them all
-    require('load-grunt-tasks')(gruntConfig);
-    //run watch by itself.
-
-//gruntConfig
-    gruntConfig.registerTask('build', ['jshint','karma', 'clean', 'copy', 'sass']);
+  gruntConfig.registerTask('build', ['jshint','karma', 'clean', 'copy', 'sass']);
 };
---
+++ test/specs/thoughter.spec.js
@@ -0,0 +1,108 @@
+(function() {
+  'use strict';
+
+  let expect = window.chai.expect;
+
+  let firstObj = {};
+  firstObj.content = 'first thought';
+  firstObj.createTime = new Date();
+  let timeObj = firstObj.createTime;
+  firstObj.id = 1111;
+  let secObj = {};
+  secObj.content = 'thought';
+  secObj.createTime = new Date();
+  secObj.id = '2222';
+  let thirdObj = {};
+  thirdObj.content = 'thought';
+  thirdObj.createTime = new Date();
+
+  describe('recent-thoughts module', function() {
+
+    describe('showRecent function', function() {
+
+      beforeEach(function() {
+        let mainTag = document.createElement('main');
+        mainTag.classList.add('recent');
+        document.querySelector('body').appendChild(mainTag);
+      });
+
+      afterEach(function() {
+          let mainTag = document.querySelector('main.recent');
+          mainTag.parentNode.removeChild(mainTag);
+      });
+
+      it('should test that there is a function', function() {
+          expect(window.thoughter.showRecent).to.be.a('function');
+      });
+
+      it('should not be an array that is passed as an argument', function() {
+          expect(window.thoughter.showRecent('blue')).to.be.an('undefined');
+          // console.info(window.thoughter.showRecent);
+      });
+
+      it('should be undefined if an array containing something other than an object is passed',
+          function() {
+              expect(window.thoughter.showRecent(['green'])).to.be.an('undefined');
+          });
+
+      it('should expect a length of 0 if nothing is passed', function(){
+          window.thoughter.showRecent(); //
+          let article = document.querySelectorAll('main article');
+          // console.info('Expect  0 = ' + article);
+          expect(article.length).to.equal(0);
+      });
+
+      it('should create a new article for every thought', function(){
+          window.thoughter.showRecent([firstObj, secObj]);
+          let recent = document.querySelector('.recent');
+          expect(recent.childNodes.length).to.equal(2);
+      });
+
+      it('should create a main with a class of recent', function(){
+          window.thoughter.showRecent([firstObj]);
+          let mainClass = document.querySelector('main').classList;
+          expect(mainClass.contains('recent'));
+      });
+
+      it('should create an article with a class of panel', function(){
+          window.thoughter.showRecent([firstObj]);
+          let articleClass = document.querySelector('article').classList;
+          // console.info(articleClass);
+          expect(articleClass.contains('panel'));
+      });
+
+      it('should create an article with a class of panel-info', function(){
+          window.thoughter.showRecent([firstObj]);
+          let articleClass = document.querySelector('article').classList;
+          // console.info(articleClass);
+          expect(articleClass.contains('panel-info'));
+      });
+
+      it('should create an header with a class of panel-heading', function(){
+          window.thoughter.showRecent([firstObj]);
+          let headingClass = document.querySelector('header').classList;
+          expect(headingClass.contains('panel-heading'));
+      });
+
+      it('should check the id for the obj given', function() {
+          window.thoughter.showRecent([firstObj]);
+          let objId = document.querySelector('article');
+          expect(objId.getAttribute('id')).to.equal('thought-1111');
+      });
+
+      it('should check content of the <p>', function() {
+          window.thoughter.showRecent([firstObj]);
+          let mContent = document.querySelector('p');
+          expect(mContent.innerHTML).to.equal('first thought');
+      });
+
+      it('should check date posted', function() {
+          window.thoughter.showRecent([firstObj]);
+          let header = document.querySelector('header');
+          // console.info(timeObj);
+          expect(header.innertext).to.equal(timeObj.value);
+        });
+      });
+    });
+
+  }());
